c1cc6d8ed2e3cbc45de5c12d1860d910428ce5b7
Mishan20 <ishansenanayaka1@gmail.com>
2024-05-19 10:06:32 +0530

    sample

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -7,16 +7,25 @@
 <form method="POST" action="{{ url('/student/edit/'. $student->id)}}">
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
     <div class=" mb-3">
-        <label for=name" class="form-label">Name</label>
-        <input value="{{$student->name}}" type="text" class="form-control" id="name" name="name" aria-describedby="">
+        <label for="name" class="form-label">Name</label>
+        <input value="{{old('name') ?? $student->name}}" type="text" class="form-control" id="name" name="name" aria-describedby="">
+        @error('name')
+            <div class="color-red text-sm">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="email1" class="form-label">Email</label>
         <input value="{{$student->email}}" type="email" class="form-control" id="email1" name="email" aria-describedby="">
+        @error('email')
+            <div class="color-red text-sm">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="phone" class="form-label">Phone</label>
         <input value="{{$student->phone}}" type="text" class="form-control" id="phone" name="phone">
+        @error('phone')
+            <div class="color-red text-sm">{{$message}}</div>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Save</button>
 </form>
